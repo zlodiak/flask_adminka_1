42de6d7ad8a751843d5c6ccdd035ca06177418fa
kalinin <prozaik81-2@yandex.ru>
2019-12-05 12:56:09 +0300

    submit natepad form

--- app.py
+++ app.py
@@ -10,34 +10,38 @@ app = Flask(__name__)
 def auth():
     if 'flask_adminka_authorized_user_id' in request.cookies:
         return redirect('/admin')
+
     return render_template('auth.html')
+
 
 @app.route('/admin')
 def admin():    
     if 'flask_adminka_authorized_user_id' not in request.cookies:
         return redirect('/', code=404)
 
+    background_values = {}
     admin_forms_values = {
         'firstname': None,
         'lastname': None,
+        'notepad': None,
     }
 
     with DB_connection() as db_connect:
         db_cursor = db_connect.cursor()
-
         id_auth_user = request.cookies.get('flask_adminka_authorized_user_id')
-        req = "select * from options where user_id='" + id_auth_user + "'"
 
         try:
-            db_cursor.execute(req)
+            request_form = "select * from options where user_id='" + id_auth_user + "'"
+            db_cursor.execute(request_form)
             record = db_cursor.fetchone()
             admin_forms_values['firstname'] = record[1]
             admin_forms_values['lastname'] = record[2]
-            print('log: successfull retrieve admin_forms_values', admin_forms_values)
+            admin_forms_values['notepad'] = record[3]
+            print('log: successfull retrieve admin_forms_values', admin_forms_values, background_values)
         except:
-            print('log: failed retrieve admin_forms_values', admin_forms_values)
+            print('log: failed retrieve admin_forms_values', admin_forms_values, background_values)
         
-        return render_template('admin.html', admin_forms_values=admin_forms_values)
+        return render_template('admin.html', admin_forms_values=admin_forms_values, background_values=background_values)
 
 
 @app.route('/auth_request', methods=['POST'])
@@ -84,6 +88,31 @@ def submit_profile_request():
             except:
                 resp = Response('failed')
                 return resp
+
+
+@app.route('/submit_notepad_request', methods=['POST'])
+def submit_notepad_request():
+    with DB_connection() as db_connect:
+        db_cursor = db_connect.cursor()
+
+        notepad = request.values.get('notepad')
+        id_auth_user = request.cookies.get('flask_adminka_authorized_user_id')
+
+        req = "select * from options where user_id=" + id_auth_user
+        db_cursor.execute(req)
+        record = db_cursor.fetchone() 
+
+        if record:
+            try: 
+                req = "UPDATE options SET notepad='" + notepad + "' WHERE user_id=" + str(id_auth_user)
+                db_cursor.execute(req);
+                db_connect.commit()
+                resp = Response('submit notepad is complete')
+                return resp
+            except:
+                resp = Response('failed')
+                return resp
+
 
 if __name__ == '__main__':
     app.run()
